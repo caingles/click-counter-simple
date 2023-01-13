d1c96c4222eb67ad8f8a9c2cc2815d74a716e57c
Christian Ingles <christianarguetaaa@gmail.com>
2023-01-12 21:26:33 -0600

    Add element references in ts script

---
+++ script/app.ts
@@ -0,0 +1,4 @@
+const counterShow = document.querySelector('#counterShow')
+const decrease = document.querySelector('#decrease')
+const reset = document.querySelector('#reset')
+const increase = document.querySelector('#increase')
