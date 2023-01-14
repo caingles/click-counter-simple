5f844bb6435bc3446b43de1832a1b8838399d5b3
Christian Ingles <christianarguetaaa@gmail.com>
2023-01-13 21:14:06 -0600

    Add function that operates depending on the button

--- script/app.ts
+++ script/app.ts
@@ -2,3 +2,20 @@ const counterShow = document.querySelector('#counterShow')
 const decrease = document.querySelector('#decrease')
 const reset = document.querySelector('#reset')
 const increase = document.querySelector('#increase')
+let counterClick: number = 0;
+
+const typeCounter = (type: string) => {
+    if (type === "increase") {
+        counterClick += 1;
+        return counterClick;
+    }
+
+    if (type === "decrease") {
+        counterClick -= 1;
+        return counterClick;
+    }
+
+    if (type === "reset") {
+        return 0;
+    }
+}
--- script/build/app.js
+++ script/build/app.js
@@ -1,3 +1,19 @@
 "use strict";
 const counterShow = document.querySelector('#counterShow');
-console.log(counterShow);
+const decrease = document.querySelector('#decrease');
+const reset = document.querySelector('#reset');
+const increase = document.querySelector('#increase');
+let counterClick = 0;
+const typeCounter = (type) => {
+    if (type === "increase") {
+        counterClick += 1;
+        return counterClick;
+    }
+    if (type === "decrease") {
+        counterClick -= 1;
+        return counterClick;
+    }
+    if (type === "reset") {
+        return 0;
+    }
+};
